3b3516013f84eebeb3ac48ce5b8310e1a9a18aa4
roktim3030 <56694785+roktim3030@users.noreply.github.com>
2025-10-16 14:04:02 +0000

    Deploying to gh-pages from @ roktim3030/kishalayroktim@7a91c77fcbe7cd772cd0c03c11e1d1595984f25c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -462,8 +462,8 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
+          section: "News",},{id: "projects-mechanical-and-thermal-stress-analysis-of-different-types-of-memory-chips",
+          title: 'Mechanical and Thermal Stress Analysis of Different Types of Memory Chips',
           description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/kishalayroktim/projects/1_project/";
