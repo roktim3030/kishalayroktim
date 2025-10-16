ac70123ec90db187107f18979661a1f235b488b6
roktim3030 <56694785+roktim3030@users.noreply.github.com>
2025-10-16 11:57:06 +0000

    Deploying to gh-pages from @ roktim3030/kishalayroktim@2a6ce2d10c9f3048d76631b9b4ba2ead59bced13 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -72,7 +72,18 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "/kishalayroktim/blog/";
               },
-            },{id: "post-a-post-with-plotly-js",
+            },{id: "post-",
+        
+          title: "",
+        
+        description: "",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/kishalayroktim/blog/2025/2015-03-15-formatting-and-links/";
+          
+        },
+      },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
@@ -422,17 +433,6 @@ ninja.data = [{
         handler: () => {
           
             window.location.href = "/kishalayroktim/blog/2015/images/";
-          
-        },
-      },{id: "post-a-post-with-formatting-and-links",
-        
-          title: "a post with formatting and links",
-        
-        description: "march &amp; april, looking forward to summer",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/kishalayroktim/blog/2015/formatting-and-links/";
           
         },
       },{id: "books-the-godfather",
